b545b340ed672939701b77979b9b17b23c0b164b
Tomas Konecny <34043739+konetom@users.noreply.github.com>
2021-06-08 18:21:28 +0200

    20210608

--- script/login.py
+++ script/login.py
@@ -1,3 +1,4 @@
+# Save your Plaza v4.0 login and type your key to avoid typing the login at each run (the key can be any letter):
 login_name = 'PUT_USER_NAME_HERE'
 login_password = 'PUT_USER_PASSWORD_HERE'
 key = 'PUT_KEY_HERE'
