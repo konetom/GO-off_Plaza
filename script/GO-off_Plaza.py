955c5a31a3ed7574f1a82515301a69cd228d666b
Tomas Konecny <34043739+konetom@users.noreply.github.com>
2021-08-07 18:24:35 +0200

    Included output location as a new option

--- script/GO-off_Plaza.py
+++ script/GO-off_Plaza.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-if __name__ == "__main__":
-    import os
-    from side_functions import check_and_install, cli_input, say_bye
-    if "log.txt" not in os.listdir():
-        check_and_install()
-    input_file_path, wait_period, go_cutoff, gene_minimum, output_mode = cli_input()
-    if input_file_path is not None:
-        from run_functions import run_plaza, run_revigo, run_filters
-        run_plaza(file_path=input_file_path, wait_period=wait_period, go_cutoff=go_cutoff)
-        if output_mode == 2:
-            run_revigo(wait_period=wait_period)
-            say_bye()
-        else:
-            run_revigo(wait_period=wait_period)
-            run_filters(gene_minimum=gene_minimum)
-            say_bye()
+
+import os
+from side_functions import check_and_install, cli_input, say_bye
+if "log.txt" not in os.listdir():
+    check_and_install()
+input_file_path, wait_period, go_cutoff, gene_minimum, output_mode, *balast = cli_input()
+if input_file_path is not None:
+    from run_functions import run_plaza, run_revigo, run_filters
+    run_plaza(file_path=input_file_path, wait_period=wait_period, go_cutoff=go_cutoff)
+    if output_mode == 2:
+        run_revigo(wait_period=wait_period)
+        say_bye()
+    elif output_mode == 3:
+        run_revigo(wait_period=wait_period)
+        run_filters(gene_minimum=gene_minimum)
+        say_bye()
+    else:            
+        say_bye()
